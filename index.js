173f6bc5daee48f1ff64f2348a7ca55cef092e05
Divyesh Kathe <53042607+divyeshkathe@users.noreply.github.com>
2023-02-14 17:18:19 +0530

    initial

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const cloudinary = require('cloudinary').v2;
+const axios = require('axios');
+const moment = require('moment');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 4040;
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// cloudinary configuration 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+async function WebSiteScreenShot(website_url) {
+
+  const filename = moment().unix();
+  const filepath = __dirname + `/uploads/${filename}.jpg`;
+
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1940, height: 1080 });
+  await page.goto(website_url, { waitUntil: 'networkidle0' });
+
+  await page.screenshot({
+    path: filepath,
+    fullPage: true
+  });
+
+  await browser.close();
+
+  return { filename, filepath };
+}
+
+app.post('/upload', async (req, res) => {
+
+  let { url } = req.body;
+
+  const { filename, filepath } = await WebSiteScreenShot(url);
+
+  const cloud_res = cloudinary.uploader.upload(filepath, { public_id: filename });
+  cloud_res.then(async (data) => {
+    console.log(data)
+    fs.unlink(filepath, (err) => { console.log(err) });
+    res.json({
+      error: false
+    })
+
+  }).catch((err) => {
+    res.json({
+      error: true,
+    });
+  });
+
+
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 'Hello, This is a server!'
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to http://localhost:${PORT}`);
+})
