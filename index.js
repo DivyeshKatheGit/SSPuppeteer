ecf286aefcd77f2d2673e0150f53be034ea3ef7c
Divyesh Kathe <53042607+divyeshkathe@users.noreply.github.com>
2023-02-15 18:11:02 +0530

    updated exception message

--- index.js
+++ index.js
@@ -54,6 +54,20 @@ async function WebSiteScreenShot(website_url) {
     return { filename, filepath };
   }
   catch (e) {
+    console.log("error object:");
+    console.log(e);
+    console.log();
+
+    console.log("error object toString():");
+    console.log("\t" + e.toString());
+
+    console.log();
+    console.log("error object attributes: ");
+    console.log('\tname: ' + e.name + ' message: ' + e.message + ' at: ' + e.at + ' text: ' + e.text);
+
+    console.log();
+    console.log("error object stack: ");
+    console.log(e.stack);
     return false;
   }
 }
